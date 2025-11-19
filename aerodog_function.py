f27d2140e94d7db8690b257d26268033b4b0585e
Henrique de Melo Jorge Barbosa <hmj.barbosa+git@gmail.com>
2025-11-19 16:54:18 -0500

    Changed comments into Docstrings for aerodog_functions.

--- aerodog_function.py
+++ aerodog_function.py
@@ -10,44 +10,44 @@ import math
 import numpy as np
 import pandas as pd
 
-'''
+def globaltime_function(row): 
+    '''
 =============================================
 globaltime variable - concatenate Date and time 
 =============================================
 '''
-def globaltime_function(row): 
     date = row['Date(dd:mm:yyyy)']+' '+row['Time(hh:mm:ss)']
     dateframe = pd.to_datetime(date,format= '%d:%m:%Y %H:%M:%S' )
     dateformat = dateframe.strftime('%Y-%m-%d %H:%M:%S')
     return dateformat
 
-'''
+def globaltime_index(funcdata):
+    '''
 =============================================
 Function to set Date and Time as globaltime index
 =============================================
 '''
-def globaltime_index(funcdata):
     funcdata['globaltime'] = pd.to_datetime(funcdata['globaltime'],format= '%Y-%m-%d %H:%M:%S' )
     funcdata = funcdata.set_index('globaltime')
     return funcdata
 
-'''
+def reading_aeronet_data(rootdir,filetype,rawdatadir):
+    '''
 =============================================
 Function to read files from AERONET - direct-sun and inversion algorithm - level1.5 or level2.0 
 =============================================
 '''
-def reading_aeronet_data(rootdir,filetype,rawdatadir):
     inputdir = os.sep.join([rootdir,rawdatadir])
     files = [name for name in os.listdir(inputdir) if name.endswith(filetype)]
     
     return files
 
-'''
+def organizing_aeronet_data(rootdir,rawfile,filetype,use_cols,rowstoskip,rawlevel,rawdatadir,outputdir):
+    '''
 =============================================
 Function for organization of AERONET Aerosol Optical Depth (V3) - level1.5 or level2.0 data - direct sun and inversion algorithm
 =============================================
 '''
-def organizing_aeronet_data(rootdir,rawfile,filetype,use_cols,rowstoskip,rawlevel,rawdatadir,outputdir):
     inputdir = os.sep.join([rootdir,rawdatadir])
     newfiles = os.sep.join([inputdir, rawfile])
     newfilepath = newfiles.replace(rawdatadir, outputdir).replace('_'+str(rawlevel)+'.'+filetype, '.'+str(rawlevel)+'_'+filetype+'_v01')
@@ -58,12 +58,12 @@ def organizing_aeronet_data(rootdir,rawfile,filetype,use_cols,rowstoskip,rawleve
     f.insert(1,'globaltime',f.apply(globaltime_function, axis=1))
     f.to_csv(newfilepath,float_format="%.6f",index=False)  
 
-'''
+def mining_aeronet_data(rootdir,filetype,rawlevel,avgtime,rawdatadir,outputdir):
+        '''
 =============================================
 Function to concatenate direct-sun and inversion algorithm from AERONET data measurements
 =============================================
 '''        
-def mining_aeronet_data(rootdir,filetype,rawlevel,avgtime,rawdatadir,outputdir):
         inputdir = os.sep.join([rootdir,rawdatadir])
         files = [name for name in os.listdir(inputdir) if name.endswith(''.join([str(rawlevel),'.',filetype]))]
         lenfiles = len(files)
@@ -88,72 +88,72 @@ Functions to calculate aerosol optical parameters from direct-sun and inversion 
 #def ae532(row):
 #        return np.log((row["AOD_675nm"])/(row["AOD_440nm"]))/(np.log(440/675))
 
-'''AOD calculation at 532 nm'''
 def aod532(row):
+        '''AOD calculation at 532 nm'''
         return row["AOD_500nm"]*(500/532)**((-1.)*row["AE_440_675nm"])
 
-'''Angstrom Exponent calculation at 355 nm'''
 def ae355(row):
+        '''Angstrom Exponent calculation at 355 nm'''
         return np.log((row["AOD_440nm"])/(row["AOD_340nm"]))/(np.log(340/440)) 
 
-'''AOD calculation at 355 nm'''
 def aod355(row):
+        '''AOD calculation at 355 nm'''
         return row["AOD_380nm"]*(380/355)**((-1.)*row["AE_340_440nm"])
 
-'''Lidar Ratio calculation at 440 nm'''
 def lr440(row):
+        '''Lidar Ratio calculation at 440 nm'''
         return 4*(math.pi)/row['pfn180_440nm']*row['SSA_440nm']
 
-'''The LR calculation at 675 nm'''
 def lr675(row):
+        '''The LR calculation at 675 nm'''
         return 4*(math.pi)/row['pfn180_675nm']*row['SSA_675nm']
 
-'''The LR calculation at 870 nm'''
 def lr870(row):
+        '''The LR calculation at 870 nm'''
         return 4*(math.pi)/row['pfn180_870nm']*row['SSA_870nm']
 
-'''The LR calculation at 1020 nm'''
 def lr1020(row):
+        '''The LR calculation at 1020 nm'''
         return 4*(math.pi)/row['pfn180_1020nm']*row['SSA_1020nm']
 
-'''The Lidar ratio - Angstrom Exponent relation using 440 and 870 nm'''
 def lrae532(row):
+        '''The Lidar ratio - Angstrom Exponent relation using 440 and 870 nm'''
         return np.log((row["LR_870nm"])/(row["LR_440nm"]))/(np.log(440/870))
     
-'''The Lidar ratio calculation at 532 nm using LR values of 675 nm'''
 def lr532(row):
+        '''The Lidar ratio calculation at 532 nm using LR values of 675 nm'''
         return row["LR_675nm"]*(532/675)**((-1.)*row["LRAE_532nm"])
 
-'''The Lidar ratio calculation at 355 nm using LR values of 440 nm'''
 def lr355(row):
+        '''The Lidar ratio calculation at 355 nm using LR values of 440 nm'''
         return row["LR_440nm"]*(355/440)**((-1.)*row["LRAE_532nm"])
 
-'''The  spectral  variability  of  the  aerosol  single  scatterring  albedo  (dSSA) - Delta Single Scattering Albedo '''
 def dssa(row):
+   '''The  spectral  variability  of  the  aerosol  single  scatterring  albedo  (dSSA) - Delta Single Scattering Albedo '''
    return row["SSA_440nm"]-row["SSA_675nm"]
 
-'''Absorption AOD at 440 nm '''
 def aaod440(row):
+   '''Absorption AOD at 440 nm '''
    return row["AOD_440nm"]*(1. - row["SSA_440nm"])
 
-'''Absorption AOD at 675 nm '''
 def aaod675(row):
+   '''Absorption AOD at 675 nm '''
    return row["AOD_675nm"]*(1. - row["SSA_675nm"])
 
-'''Scattering AOD at 440 nm '''
 def saod440(row):
+   '''Scattering AOD at 440 nm '''
    return row["AOD_440nm"]*(row["SSA_440nm"])
 
-'''Scattering AOD at 675 nm '''
 def saod675(row):
+   '''Scattering AOD at 675 nm '''
    return row["AOD_675nm"]*(row["SSA_675nm"])
 
-'''Absorption Angstrom Exponent (AAE) at 440-675 nm '''
 def aae(row):
+   '''Absorption Angstrom Exponent (AAE) at 440-675 nm '''
    return (-1)*np.log((row["AAOD_440nm"])/(row["AAOD_675nm"]))/(np.log(440/675))
 
-'''Scaterring Angstrom Exponent (SAE) at 440-675 nm ''' 
 def sae(row):
+   '''Scaterring Angstrom Exponent (SAE) at 440-675 nm ''' 
    return (-1)*np.log((row["SAOD_440nm"])/(row["SAOD_675nm"]))/(np.log(440/675)) 
 
 '''Some aerosol products, such as AOD at 355 and 532 nm, or even Lidar ratio at 532 can be calculated appplying the Angstrom power law relationship (1,2)
